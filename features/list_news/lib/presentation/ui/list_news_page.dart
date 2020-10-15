53206e393684823296bd12843e0fb39b54f83ecc
Putra Utama <nyoman.utama@amarbank.co.id>
2020-10-15 11:52:42 +0700

    feat(feature-list-news): add hero tag on list news page and transitionBetweenRoutes

--- features/list_news/lib/presentation/ui/list_news_page.dart
+++ features/list_news/lib/presentation/ui/list_news_page.dart
@@ -52,6 +52,8 @@ class ListNewsPage extends StatelessWidget {
   Widget _buildIos(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
+        heroTag: "list_new_page",
+        transitionBetweenRoutes: false,
         middle: Text(Modular.get<LocaleKeys>().listNewsTitle.tr()),
       ),
       child: SafeArea(child: _buildList()),
