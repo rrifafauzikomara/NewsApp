57e470d1710b0dcce4f24dabeb5badde4c5745c3
malibayram <malibayram91@gmail.com>
2023-12-12 21:17:14 +0300

    Örnek PR gönderimi için

--- lib/ui/launcher/main-dev.dart
+++ lib/ui/launcher/main-dev.dart
@@ -1,14 +1,14 @@
 import 'package:core/core.dart';
 import 'package:core/network/api_constant.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:news_app/bloc/news_bloc_observer.dart';
 import 'package:news_app/ui/launcher/app_config.dart';
 import 'package:news_app/ui/launcher/news_app.dart';
 import 'package:shared/shared.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   Bloc.observer = NewsBlocObserver();
@@ -20,3 +20,5 @@ void main() async {
   Modular.init(SharedModule());
   runApp(ModularApp(module: AppModule()));
 }
+
+// Örnek PR gönderimi için
